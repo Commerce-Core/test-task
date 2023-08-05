68ec77add130a068940ca0cea3059e134f600974
vaclova2020 <vaclovas.lapinskis@gmail.com>
2023-08-06 02:37:38 +0300

    remove HasFactory

--- app/Models/PageRank.php
+++ app/Models/PageRank.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PageRank extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'rank',
         'domain',
